c12559d08980cb783c51ef27b765018066d70e7b
brunoreolon <bruno.reolonn@gmail.com>
2025-07-26 11:10:38 -0300

    hotfix/comando-slash - corrigido o comando slash

--- src/bot/cogs/slash.py
+++ src/bot/cogs/slash.py
@@ -1,14 +1,12 @@
 from discord.ext import commands
-import discord
 
 class Slash(commands.Cog):
-
     def __init__(self, bot):
         self.bot = bot
 
-    @discord.app_commands.command(name="ping", description="teste")
-    async def ping(self, interaction: discord.Interaction):
-        await interaction.response.send_message("🏓 Pong! Estou online")
+    @commands.hybrid_command(name="ping", description="Teste ping")
+    async def ping(self, ctx: commands.Context):
+        await ctx.send("🏓 Pong! Estou online")
 
 async def setup(bot):
     await bot.add_cog(Slash(bot))
